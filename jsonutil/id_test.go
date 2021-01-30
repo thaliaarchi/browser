1085710362f61b84cd67bd28e5397d4f02c10257
Andrew Archibald <andrew@aarchibald.com>
2021-01-29 19:07:57 -0700

    Test GUID encode

---
+++ jsonutil/id_test.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Andrew Archibald
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package jsonutil
+
+import "testing"
+
+var guid = [16]byte{
+	0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+	0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef}
+
+const guidStr = "01234567-89ab-cdef-0123-456789abcdef"
+const bracedGUIDStr = "{01234567-89ab-cdef-0123-456789abcdef}"
+
+func TestEncodeGUID(t *testing.T) {
+	if s := EncodeGUID(guid); s != guidStr {
+		t.Errorf("EncodeGUID: got %q, want %q", s, guidStr)
+	}
+}
+
+func TestEncodeBracedGUID(t *testing.T) {
+	if s := EncodeBracedGUID(guid); s != bracedGUIDStr {
+		t.Errorf("EncodeBracedGUID: got %q, want %q", s, bracedGUIDStr)
+	}
+}
