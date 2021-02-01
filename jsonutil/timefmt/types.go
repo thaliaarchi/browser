e0d4e45c029858920fc358c094f40173b35db7dd
Andrew Archibald <andrew@aarchibald.com>
2021-02-01 13:23:27 -0700

    Simplify time UnmarshalJSON methods, add Epoch and Unit methods

--- jsonutil/timefmt/types.go
+++ jsonutil/timefmt/types.go
@@ -6,45 +6,90 @@
 
 package timefmt
 
-import "time"
+import (
+	"encoding"
+	"time"
+)
 
-// UnixSec is a time that is formatted in json as an integer
-// representing a Unix time in seconds since 1970-01-01 00:00:00 UTC.
+// UnixSec is a time that is formatted as an integer representing
+// a Unix time in seconds since 1970-01-01 00:00:00 UTC.
 type UnixSec struct{ time.Time }
 
-// UnixMilli is a time that is formatted in json as an integer
-// representing a Unix time in milliseconds since
-// 1970-01-01 00:00:00 UTC.
+// UnixMilli is a time that is formatted as an integer representing
+// a Unix time in milliseconds since 1970-01-01 00:00:00 UTC.
 type UnixMilli struct{ time.Time }
 
-// UnixMicro is a time that is formatted in json as an integer
-// representing a Unix time in microseconds since
-// 1970-01-01 00:00:00 UTC.
+// UnixMicro is a time that is formatted as an integer representing
+// a Unix time in microseconds since 1970-01-01 00:00:00 UTC.
 type UnixMicro struct{ time.Time }
 
-// UnixNano is a time that is formatted in json as an integer
-// representing a Unix time in nanoseconds since
-// 1970-01-01 00:00:00 UTC.
+// UnixNano is a time that is formatted as an integer representing
+// a Unix time in nanoseconds since 1970-01-01 00:00:00 UTC.
 type UnixNano struct{ time.Time }
 
-// WindowsSec is a time that is formatted in json as an integer
-// representing a Windows time in seconds since 1601-01-01 00:00:00 UTC.
+// WindowsSec is a time that is formatted as an integer representing
+// a Windows time in seconds since 1601-01-01 00:00:00 UTC.
 type WindowsSec struct{ time.Time }
 
-// WindowsMilli is a time that is formatted in json as an integer
-// representing a Windows time in milliseconds since
-// 1601-01-01 00:00:00 UTC.
+// WindowsMilli is a time that is formatted as an integer representing
+// a Windows time in milliseconds since 1601-01-01 00:00:00 UTC.
 type WindowsMilli struct{ time.Time }
 
-// WindowsMicro is a time that is formatted in json as an integer
-// representing a Windows time in microseconds since
-// 1601-01-01 00:00:00 UTC.
+// WindowsMicro is a time that is formatted as an integer representing
+// a Windows time in microseconds since 1601-01-01 00:00:00 UTC.
 type WindowsMicro struct{ time.Time }
 
-// WindowsNano is a time that is formatted in json as an integer
-// representing a Windows time in nanoseconds since
-// 1601-01-01 00:00:00 UTC.
+// WindowsNano is a time that is formatted as an integer representing
+// a Windows time in nanoseconds since 1601-01-01 00:00:00 UTC.
 type WindowsNano struct{ time.Time }
+
+// Epoch returns the epoch that times are relative to. Always Unix.
+func (t UnixSec) Epoch() Epoch { return Unix }
+
+// Epoch returns the epoch that times are relative to. Always Unix.
+func (t UnixMilli) Epoch() Epoch { return Unix }
+
+// Epoch returns the epoch that times are relative to. Always Unix.
+func (t UnixMicro) Epoch() Epoch { return Unix }
+
+// Epoch returns the epoch that times are relative to. Always Unix.
+func (t UnixNano) Epoch() Epoch { return Unix }
+
+// Epoch returns the epoch that times are relative to. Always Windows.
+func (t WindowsSec) Epoch() Epoch { return Windows }
+
+// Epoch returns the epoch that times are relative to. Always Windows.
+func (t WindowsMilli) Epoch() Epoch { return Windows }
+
+// Epoch returns the epoch that times are relative to. Always Windows.
+func (t WindowsMicro) Epoch() Epoch { return Windows }
+
+// Epoch returns the epoch that times are relative to. Always Windows.
+func (t WindowsNano) Epoch() Epoch { return Windows }
+
+// Unit returns the unit that times are measured in. Always Sec.
+func (t UnixSec) Unit() Unit { return Sec }
+
+// Unit returns the unit that times are measured in. Always Milli.
+func (t UnixMilli) Unit() Unit { return Milli }
+
+// Unit returns the unit that times are measured in. Always Micro.
+func (t UnixMicro) Unit() Unit { return Micro }
+
+// Unit returns the unit that times are measured in. Always Nano.
+func (t UnixNano) Unit() Unit { return Nano }
+
+// Unit returns the unit that times are measured in. Always Sec.
+func (t WindowsSec) Unit() Unit { return Sec }
+
+// Unit returns the unit that times are measured in. Always Milli.
+func (t WindowsMilli) Unit() Unit { return Milli }
+
+// Unit returns the unit that times are measured in. Always Micro.
+func (t WindowsMicro) Unit() Unit { return Micro }
+
+// Unit returns the unit that times are measured in. Always Nano.
+func (t WindowsNano) Unit() Unit { return Nano }
 
 // MarshalText implements the text.Marshaler interface.
 func (t UnixSec) MarshalText() ([]byte, error) { return Format(t.Time, Sec, Unix), nil }
@@ -93,6 +138,30 @@ func (t WindowsMicro) MarshalJSON() ([]byte, error) { return t.MarshalText() }
 
 // MarshalJSON implements the json.Marshaler interface.
 func (t WindowsNano) MarshalJSON() ([]byte, error) { return t.MarshalText() }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *UnixSec) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *UnixMilli) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *UnixMicro) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *UnixNano) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *WindowsSec) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *WindowsMilli) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *WindowsMicro) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *WindowsNano) UnmarshalJSON(data []byte) error { return unmarshalJSON(t, data) }
 
 // UnmarshalText implements the encoding.TextUnmarshaler interface.
 func (t *UnixSec) UnmarshalText(data []byte) error {
@@ -174,66 +243,9 @@ func (t *WindowsNano) UnmarshalText(data []byte) error {
 	return nil
 }
 
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *UnixSec) UnmarshalJSON(data []byte) error {
+func unmarshalJSON(v encoding.TextUnmarshaler, data []byte) error {
 	if string(data) == "null" {
 		return nil
 	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *UnixMilli) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *UnixMicro) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *UnixNano) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *WindowsSec) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *WindowsMilli) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *WindowsMicro) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (t *WindowsNano) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	return t.UnmarshalText(data)
+	return v.UnmarshalText(data)
 }
